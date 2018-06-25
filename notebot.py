41f3e93ef76cf370fe0a32d7177475fd46ed94e4
Arkady <me@arkady.org>
2018-06-25 21:30:06 +0300

    basic bot construction

--- notebot.py
+++ notebot.py
@@ -1,3 +1,32 @@
 import settings
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-print('magic goes here') # initial test
+PROXY = settings.PROXY
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def main():
+    mybot = Updater(settings.TELEGRAM_API_KEY, request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+
+    mybot.start_polling()
+    mybot.idle()
+
+
+def greet_user(bot, update):
+    text = """
+Oh hi!
+That's one small step for notesbot, one giant leap for //project_name//
+
+			"""
+    logging.info('Chatting with {}'.format(update.message.chat.username))
+    update.message.reply_text(text)
+
+if __name__ == "__main__":
+    logging.info('Bot started')
+    main()
