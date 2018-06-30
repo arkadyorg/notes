81e4330d6669c0889b2c42f4fc752f9b9ff35a3a
Arkady <me@arkady.org>
2018-06-30 22:32:20 +0300

    book level status funtions added

--- notebot.py
+++ notebot.py
@@ -1,5 +1,5 @@
 import settings
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, RegexHandler
 from telegram import ReplyKeyboardMarkup
 
 PROXY = settings.PROXY
@@ -10,42 +10,68 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     filename='bot.log'
                     )
 
-#CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
 
-reply_keyboard = [['/mybooks', '/newbook'],
-                  ['/openbook', '/deletebook', '/renamebook'],
-                  ['/whoami']]
-bookmarkup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
-poop=0
+FIRST = range(4)
 
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
 
 def main():
     mybot = Updater(settings.TELEGRAM_API_KEY, request_kwargs=PROXY)
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(CommandHandler("whoami", userinfo))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', greet_user)],
 
+        states={
+            FIRST: [CommandHandler('openbook', booklist),
+            CommandHandler('newbook', newbook),
+            CommandHandler('deletebook', deletebook),
+            CommandHandler('home', greet_user)]
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    # log all errors
+    dp.add_error_handler(error)
     mybot.start_polling()
     mybot.idle()
 
 
 def greet_user(bot, update):
-    text = """
-Oh hi!
-That's one small step for notesbot, one giant leap for //project_name//
-/start
-
-			"""
+    reply_keyboard = [['/openbook', '/newbook', '/deletebook'],
+    ['/home']]
+    update.message.reply_text(
+        'Hi! Nice to seen you, {} what shold we do?'.format(update.message.chat.username),
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     logging.info('Chatting with {}'.format(update.message.chat.username))
-    update.message.reply_text(text,reply_markup=bookmarkup)
+    return FIRST
 
 
-def userinfo(bot, update):
-    user_text = 'You are {} with id {}'. format(update.message.chat.username, update.message.chat.id)
-    print(user_text)
-    print(update)
+def booklist(bot, update):
+    user_text = 'here ist the list of your books'
     update.message.reply_text(user_text)
+
+
+def newbook(bot, update):
+    user_text = 'here I ask you how do we call it'
+    update.message.reply_text(user_text)
+
+def deletebook(bot, update):
+    user_text = 'here I ask what we delete'
+    update.message.reply_text(user_text)
+
+def cancel(bot, update):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
 
 if __name__ == "__main__":
     logging.info('Bot started')
